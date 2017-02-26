84ec6488792698d6f688b5d539179204b5187f7b
张超凡 <@qq.com>
2017-02-26 17:55:52 +0800

    Initial Commit

---
+++ ZCFCalendar/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ZCFCalendar
+//
+//  Created by 张超凡 on 17/2/26.
+//  Copyright © 2017年 张超凡. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
